9b568db0bf3bd32b0ff440436464587dad3182fa
DiTo97 <fede97.minutoli@gmail.com>
2019-04-02 15:43:30 +0200

    Display all Questions

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $questions = Question::latest()->paginate(5);
+        return view('question.show', compact('questions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Question  $question
+     * @return Response
+     */
+    public function show(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Question  $question
+     * @return Response
+     */
+    public function edit(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Question  $question
+     * @return Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Question  $question
+     * @return Response
+     */
+    public function destroy(Question $question)
+    {
+        //
+    }
+}
